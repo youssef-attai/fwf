49c719f8eca70e7ef1219f8c43602187f7824ee1
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-06-16 10:22:03 +0300

    Implement basic component drawing
    Every component is just a rectangle

--- fwif.c
+++ fwif.c
@@ -10,3 +10,43 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+// Function to draw the window content based on the received message
+void drawWindowContent(Display *display, Window window, const char *message) {
+  // Clear the window
+  XClearWindow(display, window);
+
+  // Read JSON components from the message and draw them on the window
+  struct json_object *json = json_tokener_parse(message);
+  struct json_object *components;
+  json_object_object_get_ex(json, "components", &components);
+  int components_count = json_object_array_length(components);
+
+  struct json_object *component; // Component iterator
+
+  // Iterate over the components
+  for (int i = 0; i < components_count; i++) {
+    component = json_object_array_get_idx(components, i); // Get the component
+
+    // Get the component's x and y position
+    struct json_object *x;
+    json_object_object_get_ex(component, "x", &x);
+    struct json_object *y;
+    json_object_object_get_ex(component, "y", &y);
+
+    // Get the component's width and height
+    struct json_object *width;
+    json_object_object_get_ex(component, "width", &width);
+    struct json_object *height;
+    json_object_object_get_ex(component, "height", &height);
+
+    XFillRectangle(display, window, DefaultGC(display, DefaultScreen(display)),
+                   json_object_get_int(x), json_object_get_int(y),
+                   json_object_get_int(width), json_object_get_int(height));
+
+  // Free the JSON object
+  json_object_put(json);
+
+  // Flush the display to show the changes
+  XFlush(display);
+}
