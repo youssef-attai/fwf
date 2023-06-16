f8d3a63df3bac59be22178602292dbafacbf9f6c
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-06-16 10:29:40 +0300

    Draw component border conditionally
    To fix 1px border width when it actually should be 0px.

--- fwif.c
+++ fwif.c
@@ -77,6 +77,7 @@ void drawWindowContent(Display *display, Window window, const char *message) {
                        json_object_get_int(border_width), LineSolid, CapButt,
                        JoinBevel);
 
+    if (json_object_get_int(border_width) > 0) {
       XSetForeground(display, DefaultGC(display, DefaultScreen(display)),
                      json_object_get_int(border_red) << 16 |
                          json_object_get_int(border_green) << 8 |
@@ -85,6 +86,7 @@ void drawWindowContent(Display *display, Window window, const char *message) {
                      DefaultGC(display, DefaultScreen(display)),
                      json_object_get_int(x), json_object_get_int(y),
                      json_object_get_int(width), json_object_get_int(height));
+    }
   }
 
   // Free the JSON object
