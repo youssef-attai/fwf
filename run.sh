21e0b3637ca42340e56ac39abfd465b0c3513ac4
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-06-18 15:41:33 +0300

    Setup named pipe paths

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ gcc -o fwif fwif.c -lX11 -ljson-c
 
 # Environment variables
 export PYTHONPATH=$(pwd)
+export FWIF_WRITE_PIPE=/tmp/fwif_write_pipe
+export FWIF_READ_PIPE=/tmp/fwif_read_pipe
 
 # Run
 python examples/dynamic_list_app.py
