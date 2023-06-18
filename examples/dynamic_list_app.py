cc6179ee6f51fd5a550b333757f8cffcbbb0b2af
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-06-18 15:40:59 +0300

    Override components method and keep track of items

--- examples/dynamic_list_app.py
+++ examples/dynamic_list_app.py
@@ -5,6 +5,7 @@ from fwif import BaseApp, Component, Color
 class App(BaseApp):
     def __init__(self):
         super().__init__()
+        self.items: list[Component] = []
         self.selected_index = -1
 
     def setup_keybindings(self):
@@ -24,40 +25,40 @@ class App(BaseApp):
         self.keybindings.add('c', self.change_color)
 
     def change_color(self):
-        if self.selected_index >= 0 and len(self.components) > 0:
-            component = self.components[self.selected_index]
+        if self.selected_index >= 0 and len(self.items) > 0:
+            component = self.items[self.selected_index]
             component.background = self.random_color()
             self.update_items()
 
     def delete_from_selection_to_first(self):
-        if self.selected_index >= 0 and len(self.components) > 0:
+        if self.selected_index >= 0 and len(self.items) > 0:
             for _ in range(self.selected_index + 1):
-                self.components.pop(0)
+                self.items.pop(0)
             self.selected_index = 0
             self.update_items()
 
     def delete_from_selection_to_last(self):
-        if self.selected_index >= 0 and len(self.components) > 0:
-            for _ in range(self.selected_index, len(self.components)):
-                self.components.pop()
+        if self.selected_index >= 0 and len(self.items) > 0:
+            for _ in range(self.selected_index, len(self.items)):
+                self.items.pop()
             self.selected_index = min(
-                self.selected_index, len(self.components) - 1)
+                self.selected_index, len(self.items) - 1)
             self.update_items()
 
     def select_first(self):
-        if len(self.components) > 0:
+        if len(self.items) > 0:
             self.selected_index = 0
             self.update_items()
 
     def select_last(self):
-        if len(self.components) > 0:
-            self.selected_index = len(self.components) - 1
+        if len(self.items) > 0:
+            self.selected_index = len(self.items) - 1
             self.update_items()
 
     def add_item(self):
         item = Component(
             x=10,
-            y=30 * (len(self.components) + 1),
+            y=30 * (len(self.items) + 1),
             width=150,
             height=50,
             background=self.random_color(),
@@ -67,24 +68,24 @@ class App(BaseApp):
             text="",
             font_size=20,
         )
-        self.components.append(item)
-        self.selected_index = len(self.components) - 1
+        self.items.append(item)
+        self.selected_index = len(self.items) - 1
         self.update_items()
 
     def delete_all(self):
-        self.components = []
+        self.items = []
         self.selected_index = -1
         self.update_items()
 
     def delete_item(self):
-        if self.selected_index >= 0 and len(self.components) > 0:
-            self.components.pop(self.selected_index)
+        if self.selected_index >= 0 and len(self.items) > 0:
+            self.items.pop(self.selected_index)
             self.selected_index = min(
-                self.selected_index, len(self.components) - 1)
+                self.selected_index, len(self.items) - 1)
             self.update_items()
 
     def move_selection_down(self):
-        if self.selected_index < len(self.components) - 1:
+        if self.selected_index < len(self.items) - 1:
             self.selected_index += 1
             self.update_items()
 
@@ -94,14 +95,14 @@ class App(BaseApp):
             self.update_items()
 
     def move_item_down(self):
-        if self.selected_index < len(self.components) - 1:
-            self.components[
+        if self.selected_index < len(self.items) - 1:
+            self.items[
                 self.selected_index
-            ], self.components[
+            ], self.items[
                 self.selected_index + 1
-            ] = self.components[
+            ] = self.items[
                 self.selected_index + 1
-            ], self.components[
+            ], self.items[
                 self.selected_index
             ]
 
@@ -110,13 +111,13 @@ class App(BaseApp):
 
     def move_item_up(self):
         if self.selected_index > 0:
-            self.components[
+            self.items[
                 self.selected_index
-            ], self.components[
+            ], self.items[
                 self.selected_index - 1
-            ] = self.components[
+            ] = self.items[
                 self.selected_index - 1
-            ], self.components[
+            ], self.items[
                 self.selected_index
             ]
 
@@ -124,7 +125,7 @@ class App(BaseApp):
             self.update_items()
 
     def update_items(self):
-        for i, component in enumerate(self.components):
+        for i, component in enumerate(self.items):
             component: Component
             component.x = 10
             component.y = 30 * (i + 1)
@@ -140,44 +141,32 @@ class App(BaseApp):
 
         return Color(r, g, b)
 
-    def view(self):
-        view = super().view()
-        view["components"].insert(0, {
-            "x": 190,
-            "y": 10,
-            "width": 200,
-            "height": 200,
-            "background_color": {
-                "r": 255,
-                "g": 255,
-                "b": 255
-            },
-            "foreground_color": {
-                "r": 0,
-                "g": 0,
-                "b": 0
-            },
-            "border_color": {
-                "r": 0,
-                "g": 0,
-                "b": 0
-            },
-            "border_width": 0,
-            "text": "Press 'a' to add an item,\n"
-            "'d' to delete an item,\n"
-            "'D' to delete all items,\n"
-            "'k' to move selection up,\n"
-            "'j' to move selection down,\n"
-            "'g' to select first,\n"
-            "'G' to select last,\n"
-            "'K' to move item up,\n"
-            "'J' to move item down,\n"
-            "'c' to change color,\n"
-            "'q' to quit.",
-            "font_size": 50,
-        })
-        print(view)
-        return view
+    def components(self):
+        return [
+            Component(
+                x=190,
+                y=10,
+                width=200,
+                height=200,
+                background=Color(255, 255, 255),
+                foreground=Color(0, 0, 0),
+                border_color=Color(0, 0, 0),
+                border_width=0,
+                text="Press 'a' to add an item,\n"
+                "'d' to delete an item,\n"
+                "'D' to delete all items,\n"
+                "'k' to move selection up,\n"
+                "'j' to move selection down,\n"
+                "'g' to select first,\n"
+                "'G' to select last,\n"
+                "'K' to move item up,\n"
+                "'J' to move item down,\n"
+                "'c' to change color,\n"
+                "'q' to quit.",
+                font_size=50,
+            ),
+            *self.items
+        ]
 
 
 def main():
